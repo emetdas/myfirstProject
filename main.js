de5ebc94aa0e2aac3c332e2f5b01d02ba1b41c8b
emetdas <emet@b2bleadstaff.com>
2021-04-05 15:48:14 +0600

    some update

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let gallery_manue = document.querySelector(".gallery-link-manue");
 let gallery_link = document.querySelectorAll(".gallery-link");
 let mobile_icon = document.querySelector(".mobile-icon");
+let gallery_img = document.querySelectorAll(".gallery-img");
 // mobile-manue
 mobile_icon.addEventListener("click", () => {
   mobile_icon.classList.toggle("active");
 });
-// mobile-manue
 // mobile-manue
 // active gallery manue
 gallery_manue.addEventListener("click", (e) => {
@@ -19,6 +19,16 @@ gallery_manue.addEventListener("click", (e) => {
   if (clicked) {
     clicked.classList.add("active-gallery");
   }
+  let filterName = clicked.getAttribute("data-gallery");
+  gallery_img.forEach((img) => {
+    let filterImages = img.getAttribute("data-gallery");
+    if (filterImages == filterName || filterName == "all") {
+      img.classList.remove("hide");
+      img.classList.add("show");
+    } else {
+      img.classList.add("hide");
+      img.classList.remove("show");
+    }
+  });
 });
-
 // active gallery manue
